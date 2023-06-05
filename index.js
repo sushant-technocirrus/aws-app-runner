63eb37d86247a350125060dc67117e60c54512d8
Sushant Karanjikar <122975959+sushant-technocirrus@users.noreply.github.com>
2023-06-05 16:16:19 +0530

    nodejs app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => res.send('Welcome To Technocirrus'))
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+      console.log(`Onexlab app listening on port ${port}`);
+  });
